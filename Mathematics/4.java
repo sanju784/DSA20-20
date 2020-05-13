6c07c2991044142289a6583a59a1911b875dfd34
sanjeev chaurasia <sanju784@users.noreply.github.com>
2020-05-13 20:53:34 +0900

    Prime Factorization of a number

---
+++ Mathematics/4.java
@@ -0,0 +1,6 @@
+// Prime Factorization of a no. e.g.  24 - 2*2*2*3
+// Of a no only one prime factorization is possible
+
+1. Run a loop from 2 to sqrt(n)
+2. Time complexity O(n)
+3. Space Complexity O(1)
