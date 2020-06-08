ddf0b7702cb7fecca9f6eccc5e1fc528e1522aca
sanju784 <sanjunitr51@gmail.com>
2020-06-08 20:59:16 +0900

    Area of Polygon

--- 0.Mathematics/6.java
+++ 0.Mathematics/6.java
@@ -1,3 +1,4 @@
 // To find area of a polygon, we use cross product formula to find area of a triangle when one vertex of the triangle is (0,0)
 
 cross_product_area = (Ax*By - Ay*Bx) / 2; // Here A & B are two points of the triangle, whereas the third point is the origin i.e. (0,0)
+
